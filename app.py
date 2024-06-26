b2191ce8c861c56b39c547843c0856a7f75d10df
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 17:32:49 +0200

    server: old version

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_jwt_extended import JWTManager
-# from flask_cors import CORS
-# from flask_socketio import SocketIO
-
+from flask_sqlalchemy import SQLAlchemy
 
 # file api import
 from api.admin import setup_admin
 from api.routes import api
 # from api.model import db
-# from api.config import Config
 
+# App - server
 
-# App initialization
 app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "DB_URL"
 
+# # Initialize SQLAlchemy and defining a simple Book model
+# db = SQLAlchemy(app)
+
+# class Book(db.Model):
+#     book_id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String)
+
+# # Create database tables in the PostgreSQL database
+# with app.app_context():
+#     db.create_all()
 
 #setting
 app.register_blueprint(api, url_prefix='/api')
-# app.config.from_object(Config)    
 setup_admin(app)
-# CORS(app)
-
 
 # Interface
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-    # with app.app_context():
-        # db.create_all()
