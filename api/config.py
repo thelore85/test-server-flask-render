ce3448aceccc911628d8a7b5237513ccd7b4210b
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 20:09:43 +0200

    server: deleting postgres DB_URL - testing if solve error

--- api/config.py
+++ api/config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///database.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_COOKIE_SECURE = False
     JWT_SECRET_KEY = os.getenv("JWT_KEY")
