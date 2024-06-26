a4ebea8dfca350589164ebcf9ea1c9e486e669c6
TheLore85 <garofalo.digital@gmail.com>
2024-06-26 16:10:14 +0200

    server: app.py setup

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# file api import
+from api.admin import setup_admin
+from api.routes import api
+from api.model import db
+
+# App - server
+app = Flask(__name__)
+
+# # // Initialize SQLAlchemy and defining a simple Book model
+# app.config["SQLALCHEMY_DATABASE_URI"] = "DB_URL"
+# db = SQLAlchemy(app)
+
+# class Book(db.Model):
+#     book_id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String)
+
+# with app.app_context():
+    # db.create_all()
+
+
+#setting
+app.register_blueprint(api, url_prefix='/api')
+setup_admin(app)
+
+# Interface
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
