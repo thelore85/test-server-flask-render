85ef33f99bf5d7b637d139ee1fca86a9b6eec696
TheLore85 <garofalo.digital@gmail.com>
2024-06-27 10:44:22 +0200

    app: remuving SQL alquemi import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+# flask import
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_cors import CORS
